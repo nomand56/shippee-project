7f70b7969afcd3ae4660058c07b2f39ea143fa04
devqof1 <dev1.devqod@gmail.com>
2023-05-21 22:04:46 +0500

    updated shippee

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', routes);
 app.use('/uploads', express.static('uploads'));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+  });
 // app.use('/', (req, res) => {
 //     res.send(`
 //   <h1>Welcome to E-commerce Rest APIs</h1>
